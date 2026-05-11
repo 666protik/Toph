f2a6415b88d9330b605770e0ca4c56a0d6c789c6
PROTIK ROY <666protik@gmail.com>
2026-05-11 09:43:39 +0000

    Update Big Factorials.py

--- Big Factorials.py
+++ Big Factorials.py
@@ -1,3 +1,18 @@
+'''
+Given an integer N, print the trailing 4 digits of N! (N factorial).
+
+    N	    Factorial (N!N!)	    Last 4 Digits
+    3	    6	                           6
+    7	    5040	                    5040
+    11  	39916800	                6800
+    15	    1307674368000           	8000
+
+    Input           Output
+      4               24  
+      10             8800
+
+'''
+
 import math
 
 f = math.factorial(int(input().strip()))
