905876bb19d118573527722556c42bdbef5372bb
PROTIK ROY <666protik@gmail.com>
2026-05-15 14:54:38 +0000

    Create Formatted Numbers.py

---
+++ Formatted Numbers.py
@@ -0,0 +1,10 @@
+'''
+Read an integer variable and print it in which the digits are separated
+into groups of three by commas.
+Input: 1171123
+Output: 1,171,123
+'''
+
+A=int(input())
+B=f"{A:,}"
+print(B)
